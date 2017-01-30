4b3f68a90297b6799720150ef81a3dc33d433460
Kartik Hegde <khegde2@illinois.edu>
2017-01-29 21:37:25 -0800

    Add to init

--- Perceptron.py
+++ Perceptron.py
@@ -1,1 +1,10 @@
 import numpy as np 
+class Perceptron(object):
+	def __init__(self):
+		print "init"
+	def fit(self):
+		print "fit"
+	def net_input(self):
+		print "net input"
+	def predict(self):
+		print "predict" 
